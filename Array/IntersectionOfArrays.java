3d97ace566fe4323d74160154e05d1ff03396969
higoldi31 <goldiikumari71@gmail.com>
2025-09-22 22:09:00 +0530

    Added new file to Array

---
+++ Array/IntersectionOfArrays.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class IntersectionOfArrays {
+    public static void main(String[] args) {
+        int[] arr1={2,4,1,8,6};
+        int[] arr2={4,9,1};
+        int[] res=findIntersection(arr1,arr2); //[4,1]
+        System.out.println(Arrays.toString(res));
+    }
+    public static int[] findIntersection(int[] arr1,int[] arr2){
+        HashSet<Integer> set=new HashSet<>();
+        HashSet<Integer> resultSet=new HashSet<>();
+
+        for(int items:arr1){
+            set.add(items);
+        }
+
+        for(int items:arr2){
+            if(set.contains(items)){
+                resultSet.add(items);
+            }
+        }
+        int[] result=new int[resultSet.size()];
+        int index=0;
+        for(int items:resultSet){
+            result[index++]=items;
+        }
+        return result;
+    }
+}
